f8333499c6342b150f18c9fb94083f4fc88316e2
prattvaidya <pratt.vaidya@gmail.com>
2021-06-10 11:23:33 -0700

    update todo

--- app/todo.tsx
+++ app/todo.tsx
@@ -1,12 +1,19 @@
 import React from "react"
 import { Text } from "react-native"
+import firestore from "@react-native-firebase/firestore"
 import { List } from "react-native-paper"
 
 function Todo({ id, title, complete }) {
+  async function toggleComplete() {
+    await firestore().collection("todos").doc(id).update({
+      complete: !complete,
+    })
+  }
+
   return (
     <List.Item
       title={title}
-      onPress={() => {}}
+      onPress={() => toggleComplete()}
       right={(props) => <Text {...props} children={complete ? "(complete)" : ""} />}
     />
   )
