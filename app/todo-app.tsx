22fc0726cca0fcde4385aeed9ec48cbe844d7a32
prattvaidya <pratt.vaidya@gmail.com>
2021-06-10 10:30:08 -0700

    implement UI for todo list

--- app/todo-app.tsx
+++ app/todo-app.tsx
@@ -1,17 +1,31 @@
 import React from "react"
-import { Text, View, ViewStyle } from "react-native"
+import { ScrollView, Text } from "react-native"
 
-const CENTER: ViewStyle = {
-  flex: 1,
-  justifyContent: "center",
-  alignItems: "center",
-}
+import { initialWindowMetrics } from "react-native-safe-area-context"
+
+import firestore from "@react-native-firebase/firestore"
+import { Appbar, TextInput, Button } from "react-native-paper"
 
 function Todos() {
+  const ref = firestore().collection("todos")
+
+  const {
+    insets: { top, bottom },
+  } = initialWindowMetrics
+
   return (
-    <View style={CENTER}>
-      <Text>Hello</Text>
-    </View>
+    <>
+      <Appbar style={{ marginTop: top }}>
+        <Appbar.Content title={"TODOs List"} />
+      </Appbar>
+      <ScrollView style={{ flex: 1, padding: 10 }}>
+        <Text>List of TODOs!</Text>
+      </ScrollView>
+      <TextInput label={"New Todo"} textAlign="left" onChangeText={() => {}} />
+      <Button style={{ marginBottom: bottom }} onPress={() => {}}>
+        Add TODO
+      </Button>
+    </>
   )
 }
 
